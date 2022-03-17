7ddab705b33068888d1fcde37ee8de283f27ebdd
odettelittle <Odettelittle1@gmail.com>
2022-03-17 00:48:08 +0000

    segundocommit

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import {Card} from './Card';
+import React from "react";
 
-export function Card () {
-    return <div>Card</div>
-
+export function Card() {
+	return (
+		<div className="card" style={{ width: "18rem;" }}>
+			<img className="card-img-top" src="..." alt="Card image cap" />
+			<div className="card-body">
+				<h5 className="card-title">Card title</h5>
+				<p className="card-text">
+					Some quick example text to build on the card title and make
+					up the bulk of the card's content.
+				</p>
+				<a href="#" className="btn btn-primary">
+					Go somewhere
+				</a>
+			</div>
+		</div>
+	);
 }
-
-export const Card = () => 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,13 +4,30 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 import { Jumbotron } from "./Jumbotron";
+import { Navbar } from "./Navbar";
+import { Card } from "./Card";
 
 //create your first component
 const Home = () => {
 	return (
-		<div>
+		<>
+			<Navbar />
 			<Jumbotron />
-		</div>
+			<div className="row">
+				<div className="col-3">
+					<Card title="first title" text="some text" />
+				</div>
+				<div className="col-3">
+					<Card />
+				</div>
+				<div className="col-3">
+					<Card />
+				</div>
+				<div className="col-3">
+					<Card />
+				</div>
+			</div>
+		</>
 	);
 };
 
