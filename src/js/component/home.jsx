b36cf53f021a858f0683aa82edbeb8d73eff88f1
odettelittle <Odettelittle1@gmail.com>
2022-03-19 00:33:02 +0000

    octavocommit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -17,25 +17,26 @@ const Home = () => {
 				<div className="col-sm-12 col-md-6 col-lg-3">
 					<Card
 						title="First title"
-						text="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+						text="Few places in the world are as married to the water as Venice. 
+						Not only has the Floating City replaced streets with canals and land with islands."
 					/>
 				</div>
 				<div className="col-sm-12 col-md-6 col-lg-3">
 					<Card
 						title="Second title"
-						text="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+						text="Like much of the sea-hugging world, the city is also facing an existential threat as the waters rise and its ground sinks."
 					/>
 				</div>
 				<div className="col-sm-12 col-md-6 col-lg-3">
 					<Card
 						title="Third title"
-						text="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+						text="“Having a floating home used to be something only for vacationers or the uber-wealthy,” Mr. Funk said. “Now more people are realizing they can do it.“"
 					/>
 				</div>
 				<div className="col-sm-12 col-md-6 col-lg-3">
 					<Card
 						title="Fourth title"
-						text="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+						text="“It really is the perfect studio space,” he said. “It has all the inspiration and little of the distraction.”"
 					/>
 				</div>
 			</div>
